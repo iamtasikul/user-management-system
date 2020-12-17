c2e45ea3a865d7bdc5c3b518cfac160fed589af2
Tasikul Islam <iamtasikul@gmail.com>
2020-12-17 15:59:18 +0530

    Load Static Files

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv').config({ path: '.env' });
+const path = require('path');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
 
 const app = express();
+
+// log requests
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/', (req, res) => {
   res.send('Use Management System');
