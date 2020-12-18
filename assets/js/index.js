32a04e4c07c8da525a19870993a9e644fcfdf87d
Tasikul Islam <iamtasikul@gmail.com>
2020-12-18 16:36:27 +0530

    Delete user Data of  MongoDB from client

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,3 +22,22 @@ $('#update_user').submit(function (event) {
     alert('Data Updated Successfully!');
   });
 });
+
+if (window.location.pathname == '/') {
+  $ondelete = $('.table tbody td a.delete');
+  $ondelete.click(function () {
+    var id = $(this).attr('data-id');
+
+    var request = {
+      url: `http://localhost:5000/api/users/${id}`,
+      method: 'DELETE',
+    };
+
+    if (confirm('Do you really want to delete this record?')) {
+      $.ajax(request).done(function (response) {
+        alert('Data Deleted Successfully!');
+        location.reload();
+      });
+    }
+  });
+}
